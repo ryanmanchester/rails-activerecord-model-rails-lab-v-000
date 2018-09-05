45bb22e07a837ada73945bc98154d8cb48bc80cd
Ryan Manchester <ryanm17@gmail.com>
2018-09-05 21:11:14 +0000

    automatically backed up by learn

--- db/migrate/20180905211055_create_students.rb
+++ db/migrate/20180905211055_create_students.rb
@@ -2,9 +2,8 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :first_name
-      t.string :
       t.string :last_name
-      t.string :
+      
     end
   end
 end
